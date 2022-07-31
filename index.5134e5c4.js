7233f7d52a63dc588b8d2b2e75390af0370e5d42
Alexey-Romaniv <105770723+Alexey-Romaniv@users.noreply.github.com>
2022-07-31 21:57:13 +0000

    Deploying to gh-pages from @ Alexey-Romaniv/Phoenix@5bcf4845ee984c61e889fd58b03c20be3f4a38ed 🚀

---
+++ index.5134e5c4.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-mobile-menu-open]"),closeModalBtn:document.querySelector("[data-mobile-menu-close]"),modal:document.querySelector("[data-mobile-menu-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.5134e5c4.js.map
