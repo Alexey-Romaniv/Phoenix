147cf53cd18950b2e6d13ed7319b1819a7bfb82b
Alexey-Romaniv <105770723+Alexey-Romaniv@users.noreply.github.com>
2022-08-01 08:36:39 +0000

    Deploying to gh-pages from @ Alexey-Romaniv/Phoenix@bdbfd460d3d7ffe9b1f7c9c1c346288beac35839 🚀

---
+++ index.a143e616.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".reviews__slider-big").slick({asNavFor:".reviews__slider-small",arrows:!1,responsive:[{breakpoint:768,settings:{arrows:!0}}],mobileFirst:!0}),$(".reviews__slider-small").slick({slidesToShow:3,arrows:!0,responsive:[{breakpoint:768,settings:{arrows:!1,slidesToShow:7,slidesToScroll:7,infinite:!1}}],mobileFirst:!0,asNavFor:".reviews__slider-big"})}));
+//# sourceMappingURL=index.a143e616.js.map
--- index.a509747d.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$(".reviews__slider-big").slick({asNavFor:".reviews__slider-small",arrows:!1,responsive:[{breakpoint:768,settings:{arrows:!0}}],mobileFirst:!0}),$(".reviews__slider-small").slick({slidesToShow:3,arrows:!0,responsive:[{breakpoint:768,settings:{arrows:!1,slidesToShow:7}}],mobileFirst:!0,asNavFor:".reviews__slider-big"})}));
-//# sourceMappingURL=index.a509747d.js.map
