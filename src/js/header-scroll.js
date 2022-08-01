b584e0686bcca380cdf2075aa4406879721cd207
r-strilets <str.komar@gmail.com>
2022-08-02 01:49:44 +0300

    all for header

--- src/js/header-scroll.js
+++ src/js/header-scroll.js
@@ -12,23 +12,13 @@ function changeHeaderBackground() {
   }
 }
 
-// $(document).ready(function(){ 
-//   $(".nav-scroll").on("click","a", function (event) { 
-//       event.preventDefault(); 
-//       var id  = $(this).attr('href'), 
-//           top = $(id).offset().top; 
-//     $('body,html').animate({ scrollTop: top }, 1500);
-//     if (document.documentElement.clientWidth >= 1366) return;
-//     const menuBtnRef = document.querySelector("[data-menu-button]");
-//     const mobileMenuRef = document.querySelector("[data-menu]");
-//     const body = document.querySelector("#page");
-//     const expanded =
-//     menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-
-//   menuBtnRef.classList.toggle("is-open");
-//   menuBtnRef.setAttribute("aria-expanded", !expanded);
-
-//   mobileMenuRef.classList.toggle("is-open");
-//   body.classList.remove("no-scroll");
-//   }); 
-// }); 
+$(document).ready(function(){ 
+  $(".nav-scroll").on("click","a", function (event) { 
+      event.preventDefault(); 
+      var id  = $(this).attr('href'), 
+          top = $(id).offset().top; 
+    $('body,html').animate({ scrollTop: top }, 1500);
+    if (document.documentElement.clientWidth >= 1366) return;
+    
+  }); 
+}); 
